2178f31703fcbb30ad378e2e54f46050cfb7d99c
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-05 22:40:08 -0300

    Aula 6 de JS

---
+++ Aula6/script.js
@@ -0,0 +1,54 @@
+// const titulo = document.querySelector('h1')
+
+// console.log("Título representa", titulo)
+
+// titulo.textContent = "Novo título"
+
+// // const container = document.getElementById("container")
+
+// const container = document.querySelector("#container")
+
+// console.log(container)
+
+// container.innerHTML = "<p>Novo Parágrafo</p>"
+
+// const imagem = document.querySelector('img')
+// console.log('imagem')
+
+// imagem.setAttribute('src','imagens/banana.jpg')
+
+// const caixa = document.getElementsByClassName('box')
+// console.log('caixa')
+
+// const botao = document.getElementById('botao')
+// console.log(botao)
+
+// botao.addEventListener('click', () => {
+//     caixa.classList.toggle('oculto')
+// }) 
+
+// const novoItem = document.createElement('li')
+
+// novoItem.textContent = "Novo Item"
+
+// document.querySelector('ul').appendChild
+// (novoItem)
+// novoItem.textContent = new Date().getTime
+// novoItem.remove()
+
+// localStorage.setItem('nome1', 'João')
+// localStorage.setItem('nome2', 'Daniel')
+// localStorage.setItem('nome3', 'Thiago')
+
+// const usuario = {
+//     nome: 'Daniel',
+//     idade: '36'
+// }
+
+// localStorage.setItem('usuario', JSON.stringify(usuario))
+
+// const valorRecuperado = JSON.parse(localStorage.getItem('usuario'))
+
+// console.log(valorRecuperado)
+// console.log(valorRecuperado.nome)
+
